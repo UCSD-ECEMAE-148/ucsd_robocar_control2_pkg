612e27d7236de408e6578cb3be71bd3bd5af7c4c
Dominic Nightingale <djnighti@ucsd.edu>
2022-05-04 04:50:45 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
+        (os.path.join('share', package_name, 'controller_submodule'), glob('controller_submodule/*.py')),
 
     ],
     install_requires=['setuptools'],
