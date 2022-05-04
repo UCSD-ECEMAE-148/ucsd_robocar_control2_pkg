ab109077efbe77d38b10dc285b9ee81adfb45965
Dominic Nightingale <djnighti@ucsd.edu>
2022-05-04 04:52:26 +0000

    Update ucsd_robocar_control2_pkg/controller_submodule/__init__.py, setup.py

--- setup.py
+++ setup.py
@@ -13,9 +13,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
-        (os.path.join('share', package_name, 'controller_submodule'), glob('controller_submodule/*.py')),
-
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'controller_submodule'), glob('controller_submodule/*.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
