7e13679310fdefd315eb18a901a0854c328d5104
Dominic Nightingale <djnighti@ucsd.edu>
2022-05-14 09:25:55 +0000

    adding data

--- ucsd_robocar_control2_pkg/controller_submodule/linear_kalman_filter.py
+++ ucsd_robocar_control2_pkg/controller_submodule/linear_kalman_filter.py
@@ -67,13 +67,14 @@ class LinearKalmanFilter:
                 
                 
                 self.xhat_mat[:, k] = self.xhat.transpose()  # store the estimates
+                
+                # Kalman predictor gain
+                K = np.dot(np.dot(np.dot(A, self.Pp), C_t), np.linalg.inv(np.add(np.dot(np.dot(C, self.Pp), C_t), Ro))) 
 
                 # Time update
                 self.xhat = np.add(np.dot(A, self.xhat).reshape(num_states, 1),np.dot(B, u[k]))  # predicted state estimate
                 self.Pp = np.add(np.dot(np.dot(A, self.Pp), A_t), Qo)  # covariance
-
-                # # Measurement update
-                K = np.dot(np.dot(np.dot(A, self.Pp), C_t),np.linalg.inv(np.add(np.dot(np.dot(C, self.Pp), C_t), Ro)))  # Kalman predictor gain
+ 
                 self.xhat = np.add(np.dot((np.subtract(A, np.dot(K, C))), self.xhat).reshape(num_states, 1), np.add(np.dot(B, u[k]), np.dot(K, y[:, k]).reshape(num_states, 1)))
                 self.Pp = np.subtract(self.Pp, np.dot(np.dot(np.dot(np.dot(self.Pp, C_t), np.linalg.inv(np.add(np.dot(np.dot(C, self.Pp), C_t), Ro))), C), self.Pp))
 
@@ -83,6 +84,109 @@ class LinearKalmanFilter:
                 
             except:
                 pass
+            
+        if self.debug:
+            print(f"A: {A}")
+            print(f"B: {B}")
+            print(f"C: {C}")
+            print(f"D: {D}")
+            print(f"self.x0: {x0}")
+            print(f"u: {u}")
+            print(f"y: {y}")
+            print(f"K: {P0}")
+            print(f"y: {y}")
+            print(f"self.xhat: {self.xhat}")
+            print(f"u[0]: {u}")
+            print(f"np.dot(B, u[k]): {np.dot(B, u[0])}")
+            print(f"K: {K}")
+            print(f"y[:, k]: {y[:, 0]}")
+            print(f"np.dot(K, y[:, k]): {np.dot(K, y[:, 0])}")
+            print(f"np.dot(K, y[:, k]).reshape(num_states, 1)): {np.dot(K, y[:, 0]).reshape(num_states, 1)}")
+            print(f"np.add(np.dot(B, u[k]), np.dot(K, y[:, k]).reshape(num_states, 1))): {np.add(np.dot(B, u[0]), np.dot(K, y[:, 0]).reshape(num_states, 1))}")
+            print(f"self.xhat_mat: {self.xhat_mat}")
+            print(f"self.xhat: {self.xhat}")
+            print(f"A: {A}")
+            print(f"self.xhat: {self.xhat}")
+            print(f"np.dot(A, self.xhat) 2: {np.dot(A, self.xhat)}")
+            print(f"B: {B}")
+            print(f"u[k]: {u[k]}")
+            print(f"np.dot(B, u[k]): {np.dot(B, u[k])}")
+            print(f"K_mat: {self.K_mat}")
+            print(f"K: {K}")
+            print(f"y: {self.yhat}")
+            print(f"y_mat: {self.yhat_mat}")
+        return self.xhat, self.Pp
+    
+    def lkf_step(self, sys, x0, u, y, P0, Qo, Ro):
+        self.sysd = sys
+        [A, B, C, D] = ssdata(self.sysd)
+        A = np.array(A)
+        B = np.array(B)
+        C = np.array(C)
+        D = np.array(D)
+        x0 = np.array(x0)
+        u = np.array([u])
+        y = np.array(y)
+        P0 = np.array(P0)
+        Qo = np.array(Qo)
+        Ro = np.array(Ro)
+
+        a_num_rows, a_num_cols = A.shape
+        d_num_rows, d_num_cols = D.shape
+        self.Pp = P0
+        self.xhat = x0
+        self.sample_size = u.size
+        self.xhat_mat = np.zeros([a_num_rows, self.sample_size], dtype=float)
+        self.yhat_mat = np.zeros([d_num_rows, self.sample_size], dtype=float)
+        self.K_mat = np.zeros([a_num_rows, self.sample_size], dtype=float)
+
+        A_t = A.transpose()
+        C_t = C.transpose()
+        num_states = A.shape[0]
+
+        try:
+            # self.xhat_mat[:, k] = self.xhat.transpose()  # store the estimates
+            # # Time update
+            # self.xhat = np.add(np.dot(A, self.xhat).reshape(num_states, 1),np.dot(B, u[k]))  # predicted state estimate
+            # self.Pp = np.add(np.dot(np.dot(A, self.Pp), A_t), Qo)  # covariance
+            # # # Measurement update
+            # K = np.dot(np.dot(np.dot(A, self.Pp), C_t),np.linalg.inv(np.add(np.dot(np.dot(C, self.Pp), C_t), Ro)))  # Kalman predictor gain
+            # self.xhat = np.add(np.dot((np.subtract(A, np.dot(K, C))), self.xhat).reshape(num_states, 1), np.add(np.dot(B, u[k]), np.dot(K, y[:, k]).reshape(num_states, 1)))
+            # self.Pp = np.subtract(self.Pp, np.dot(np.dot(np.dot(np.dot(self.Pp, C_t), np.linalg.inv(np.add(np.dot(np.dot(C, self.Pp), C_t), Ro))), C), self.Pp))
+            # # filtered output prediction
+            # self.yhat = np.dot(C, self.xhat)
+            
+            
+            self.xhat_mat[:, k] = self.xhat.transpose()  # store the estimates
+            
+            # Kalman predictor gain
+            K = np.dot(np.dot(np.dot(A, self.Pp), C_t), np.linalg.inv(np.add(np.dot(np.dot(C, self.Pp), C_t), Ro))) 
+            
+            # filtered state estimate
+            self.xhat = np.add(np.dot((np.subtract(A, np.dot(K, C))), self.xhat).reshape(num_states, 1), np.add(np.dot(B, u[k]), np.dot(K, y[:, k]).reshape(num_states, 1)))
+            
+            
+            # Predicted error covariance
+            self.Pp = np.add(np.dot(np.dot(A, self.Pp), A_t), Qo)  # covariance
+            self.Pp = np.subtract(\
+                np.dot(np.dot(A, self.Pp), \
+                np.dot(\
+                    np.dot(\
+                        np.dot(\
+                            np.dot(self.Pp, C_t), \
+                            np.linalg.inv(
+                                np.add(\
+                                np.dot(np.dot(C, self.Pp), \
+                                C_t), \
+                            Ro)\
+                        )), \
+                    C), \
+                self.Pp))
+            
+            # filtered output prediction
+            self.yhat = np.dot(C, self.xhat)
+        except:
+            print("Kalman filter: Exception occured")
             
         if self.debug:
             print(f"A: {A}")
