602cc49ba741dff50108e88eafe824a6f5a4935f
Dominic Nightingale <djnighti@ucsd.edu>
2022-05-15 18:25:10 +0000

    adding test

--- ucsd_robocar_control2_pkg/controller_submodule/lqr_calculator.py
+++ ucsd_robocar_control2_pkg/controller_submodule/lqr_calculator.py
@@ -23,7 +23,7 @@ class LQRDesign:
         self.q2_coef = []
         self.q3_coef = []
         self.q4_coef = []
-        self.r1 = 1.0E-2
+        self.r1 = 1.0E-1
         self.compute_weight_functions()
         
     def compute_weight_functions(self):
