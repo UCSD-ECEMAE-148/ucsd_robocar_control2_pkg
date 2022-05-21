85b4c8fc3d3f34148effd232f0cc315d84a7e559
Dominic Nightingale <djnighti@ucsd.edu>
2022-05-21 13:16:53 +0000

    test filter

--- ucsd_robocar_control2_pkg/lqg_w_node.py
+++ ucsd_robocar_control2_pkg/lqg_w_node.py
@@ -378,7 +378,7 @@ class LqgController(Node):
         # normalized_delta = delta / self.delta_normalization
         # self.inf_throttle = self.min_speed - (self.min_speed - self.max_speed) / (1 - self.error_threshold)
         # speed_raw = ((self.min_speed - self.max_speed) / (1 - self.error_threshold)) * abs(normalized_delta) + self.inf_throttle
-        speed_raw = 0.5
+        speed_raw = 1.0
         speed = self.clamp(speed_raw, self.max_speed, self.min_speed)
 
         # Get Current Measurement
