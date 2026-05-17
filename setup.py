2fc02830705fd157a82464df9a28effdfa91dfe9
WinstonHChou <winston.h.chou@gmail.com>
2026-05-16 22:41:00 -0700

    fix: correct script directory variable names and update test requirements format

--- setup.py
+++ setup.py
@@ -26,7 +26,11 @@ setup(
     maintainer_email='djnighti@ucsd.edu',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=['pytest'],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
             'lqr_node = ucsd_robocar_control2_pkg.lqr_node:main',
